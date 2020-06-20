1dd3c4b82984ad11e2e050e443ba7befc35cd08d
Michael Seger <michael@macbookpro>
2020-06-21 01:04:11 +0200

    cosmetics

--- src/main/java/com/net128/oss/web/app/fileman/Servlet.java
+++ src/main/java/com/net128/oss/web/app/fileman/Servlet.java
@@ -1,22 +1,15 @@
 package com.net128.oss.web.app.fileman;
 
-import static java.nio.file.FileVisitResult.CONTINUE;
-
 import java.io.*;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.FileSystems;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Path;
-import java.nio.file.PathMatcher;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
+import java.util.*;
 
+import javax.servlet.MultipartConfigElement;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.http.HttpServlet;
@@ -31,18 +24,25 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.zeroturnaround.zip.ZipUtil;
 
-@MultipartConfig
+//@MultipartConfig
 @Configuration
+@MultipartConfig/*(
+	fileSizeThreshold = 1024 * 1024,
+	maxFileSize = 1024 * 1024 * 5,
+	maxRequestSize = 1024 * 1024 * 5 * 5)*/
 public class Servlet extends HttpServlet {
 	@Bean
-	public ServletRegistrationBean<Servlet> fileManagerBean() {
+	public ServletRegistrationBean<Servlet> fileManagerBean(MultipartConfigElement mce) {
 		ServletRegistrationBean<Servlet> bean =
-				new ServletRegistrationBean<>(new Servlet(), "/filemanager/index");
+			new ServletRegistrationBean<>(new Servlet(), "/filemanager/index");
+		bean.setMultipartConfig(mce);
 		bean.setLoadOnStartup(0);
 		return bean;
 	}
 
 	private static final int BUFFER_SIZE = 4096;
+	private static String GROUP = "ul";
+	private static String ITEM = "li";
 	private static final String ENCODING = StandardCharsets.UTF_8.name();
 
 	/**
@@ -50,53 +50,112 @@ public class Servlet extends HttpServlet {
 	 */
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		Files files = null;
-		File file = null, parent;
+		File file;
 		String path = request.getParameter("path");
 		String type = request.getContentType();
-		String search = request.getParameter("search");
 		String mode;
 
-		if(path==null||!(file=new File(path)).exists())
+		file = new File(path);
+
+		if(path==null||!file.exists()) {
 			files = new Roots();
-		else if(request.getParameter("zip")!=null) {
+		} else if(request.getParameter("zip")!=null) {
 			zipFile(file, response);
 		} else if(request.getParameter("delete")!=null) {
 			deleteFileOrDirectory(file);
 		} else if((mode=request.getParameter("mode"))!=null) {
 			changeMode(file, mode);
-		} else if(file.isFile())
+		} else if(file.isFile()) {
 			downloadFile(response, file);
-		else if(file.isDirectory()) {
-			if(search!=null&&!search.isEmpty())
-			     files = new Search(file.toPath(), search);
-			else if(type!=null && type.startsWith("multipart/form-data")) {
+		} else if(file.isDirectory()) {
+			if(type!=null && type.startsWith("multipart/form-data")) {
 				receiveUpload(file, request);
-			} else files = new Directory(file);
+			}
 		} else throw new ServletException("Unknown type of file or folder.");
-		
-		if(files!=null) {
-			listFiles(files, file, path, search, response);
+
+		if(files==null) {
+			files = new Directory(file);
 		}
+		listFiles(files, response);
+	}
+
+	private void listFiles(Files files, HttpServletResponse response) throws IOException {
+		final PrintWriter writer = response.getWriter();
+		writer.println(header());
+		writer.println(breadCrumb(files));
+		writer.print(tools());
+		File [] fileList=files.listFiles();
+		fileList = fileList!=null?fileList:new File[]{};
+		writer.print("<"+GROUP+" class=\"file-list\">");
+		writer.print(fileList(fileList, true));
+		writer.print(fileList(fileList, false));
+		writer.println("</"+GROUP+">");
+		writer.print(footer());
+		writer.flush();
 	}
 
 	private String header() {
-		return "<!DOCTYPE html><html><head><link rel=\"stylesheet\" href=\"css/file-manager.css\"></head><body>";
+		return "<!DOCTYPE html><html><head>" +
+			"<link rel=\"stylesheet\" href=\"css/file-manager.css\">" +
+			"</head><body>"
+			;
 	}
 
-	private String breadCrumb(Files files) {
-		return "<p>Current directory: "+files+"</p>";
+	private String breadCrumb(Files files) throws UnsupportedEncodingException {
+		File file=new File(files.toString());
+		List<String> parentList=new ArrayList<>();
+		parentList.add(file.getName());
+		file=file.getParentFile();
+		while(file!=null) {
+			String name = file.getName();
+			if(name.isEmpty()) {
+				name = ">";
+			}
+			String encodedPath = URLEncoder.encode(file.getAbsolutePath(), ENCODING);
+			file=file.getParentFile();
+			parentList.add(0, String.format("<a href=\"?path=%s\">%s</a>", encodedPath , name));
+		}
+		return "<p class=\"breadcrumb\"> "+String.join(" / ", parentList)+"</p>";
+	}
+
+	private String singleFile(File child) throws UnsupportedEncodingException {
+		//hide dot files
+		if (child.getName().matches("^[.]+[^.]+.*")) {
+			return "";
+		}
+		StringBuilder sb = new StringBuilder();
+		sb.append("<").append(ITEM);
+		if (child.isDirectory()) {
+			sb.append(" class=\"directory\"><a href=\"?path=")
+					.append(URLEncoder.encode(child.getAbsolutePath(), ENCODING))
+					.append("\"><i class=\"item\" ></i>").append(child.getName()).append("</a>");
+		} else {
+			sb.append(" class=\"file\"><span class=\"name\"><i class=\"item\"></i>")
+					.append(child.getName()).append("</span>");
+		}
+
+		sb.append(" <span class=\"item-tool\">")
+				.append("<a class=\"download\" href=\"?path=")
+				.append(URLEncoder.encode(child.getAbsolutePath(), ENCODING))
+				.append(child.isDirectory()?"&zip":"")
+				.append("\">&#x2B07;</a>")
+				.append("</span>")
+				.append("</").append(ITEM).append(">\n");
+		return sb.toString();
+	}
+
+	private String tools() {
+		StringBuilder sb=new StringBuilder();
+		sb.append(
+			"<form class=\"tools\" method=\"post\" enctype=\"multipart/form-data\">" +
+				" <button class=\"tool\" type=\"submit\">&#x2B06;</button>" +
+				" <input type=\"file\" name=\"file\" id=\"file\" multiple>" +
+				"</form>\n");
+		return sb.toString();
 	}
 
 	private String footer() {
 		return "</body></html>";
-	}
-
-	private String noResults(String path) throws UnsupportedEncodingException {
-		StringBuilder sb=new StringBuilder();
-		sb.append("<p id=\"no-files\">No files found. ");
-		sb.append("<a class=\"dir-navigation\" href=\"?path=").append(URLEncoder.encode(path,ENCODING)).append("\">back</a>\n");
-		sb.append(" </p>");
-		return sb.toString();
 	}
 
 	private void zipFile(File file, HttpServletResponse response) throws IOException {
@@ -137,8 +196,8 @@ public class Servlet extends HttpServlet {
 
 	private void receiveUpload(File file, HttpServletRequest request) throws IOException, ServletException {
 		for(Part part:request.getParts()) {
-			String name;
-			if((name=partFileName(part))==null) //retrieves <input type="file" name="...">, no other (e.g. input) form fields
+			String name=partFileName(part);
+			if(name==null || name.trim().length()==0) //retrieves <input type="file" name="...">, no other (e.g. input) form fields
 				continue;
 			if(request.getParameter("unzip")==null)
 				try(OutputStream output=new FileOutputStream(new File(file,name))) {
@@ -147,77 +206,13 @@ public class Servlet extends HttpServlet {
 		}
 	}
 
-	private void listFiles(Files files, File file, String path, String search, HttpServletResponse response) throws IOException {
-		final PrintWriter writer = response.getWriter();
-		writer.println(header());
-		writer.println(breadCrumb(files));
-
-		writer.print("<ul class=\"file-list\">");
-		//writer.print("<div class=\"file-list\">");
-		if(files instanceof Directory) {
-			writer.print(parentLink(file, path));
-		}
-
-		File [] fileList=files.listFiles();
-		if(fileList!=null) {
-			boolean isSearchResult =  search != null && !search.isEmpty();
-			writer.print(fileList(fileList, isSearchResult, true));
-			writer.print(fileList(fileList, isSearchResult, false));
-		}
-		else if(search!=null && !search.isEmpty() && path!=null && !path.isEmpty()) {
-			writer.print(noResults(path));
-		}
-		writer.println("</ul>");
-
-		if(!(files instanceof Roots)) {
-			writer.print(tools(search));
-		}
-		writer.print(footer());
-		writer.flush();
-	}
-
-	private String fileList(File [] fileList, boolean isSearchResult, boolean directory) throws UnsupportedEncodingException {
+	private String fileList(File[] fileList, boolean directory) throws UnsupportedEncodingException {
 		StringBuilder sb = new StringBuilder();
 		for (File child : fileList) {
 			if(directory == child.isDirectory()) {
-				sb.append(singleFile(child, isSearchResult));
+				sb.append(singleFile(child));
 			}
 		}
-		return sb.toString();
-	}
-
-	private String singleFile(File child, boolean searchResult) throws UnsupportedEncodingException {
-		//hide dot files
-		if (child.getName().matches("^[.]+[^.]+.*")) {
-			return "";
-		}
-		StringBuilder sb = new StringBuilder();
-		if (child.isDirectory()) {
-			sb.append("<li class=\"directory\"><i class=\"icon\" /></i><a href=\"?path=" + URLEncoder.encode(child.getAbsolutePath(), ENCODING) + "\" title=\"" + child.getAbsolutePath() + "\">" + child.getName() + "</a></li>");
-			//sb.append("<a class=\"download\" href=\"?path=" + URLEncoder.encode(child.getAbsolutePath(), ENCODING) + "&zip\" title=\"download\">&#8681;</a>");
-		} else {
-			sb.append("<li class=\"file\"><i class=\"icon\" /></i><span class=\"name\">" + child.getName()+"</span>");
-			sb.append("<a \"download\" href=\"?path=" + URLEncoder.encode(child.getAbsolutePath(), ENCODING) + "\" title=\"download\">&#8681;</a></li>");
-		}
-
-		if(searchResult)
-			sb.append("<span class=\"dir-navigation\"><i class=\"icon\" /></i><a href=\"?path="+URLEncoder.encode(child.getParentFile().getAbsolutePath(),ENCODING)+"\" title=\"go to parent folder\">&#128449;</a></span>");
-		sb.append("\n");
-		return sb.toString();
-	}
-
-	private String parentLink(File file, String path) throws UnsupportedEncodingException {
-		StringBuilder sb=new StringBuilder();
-		if((file.getParentFile())!=null)
-			sb.append("<span class=\"dir-navigation\"><i class=\"icon\" /></i><a href=\"?path="+URLEncoder.encode(file.getParentFile().getAbsolutePath(),ENCODING)+"\">..</a></span>\n");
-		else sb.append("<span class=\"dir-navigation\"><i class=\"icon\" /></i><a href=\"?path=\">..</a></span>\n");
-		return sb.toString();
-	}
-
-	private String tools(String search) {
-		StringBuilder sb=new StringBuilder();
-		sb.append("\n<form method=\"post\"><label for=\"search\">Search Files:</label> <input type=\"text\" name=\"search\" id=\"search\" value=\""+(search!=null?search:"")+"\"> <button type=\"submit\">Search</button></form>");
-		sb.append("<form method=\"post\" enctype=\"multipart/form-data\"><label for=\"upload\">Upload Files:</label> <button type=\"submit\">Upload</button> <button type=\"submit\" name=\"unzip\">Upload & Unzip</button> <input type=\"file\" name=\"upload[]\" id=\"upload\" multiple></form>\n");
 		return sb.toString();
 	}
 
@@ -258,29 +253,6 @@ public class Servlet extends HttpServlet {
 					}
 				};
 			} return roots;
-		}
-	}
-	protected static class Search implements Files {
-		public final Path start; public final String search;
-		private final PathMatcher matcher;
-
-		public Search(Path start, String search) {
-			this.start = start; matcher = FileSystems.getDefault().getPathMatcher("glob:"+(this.search=search));
-		}
-
-		@Override public String toString() { return start.toString(); }
-		@Override public File[] listFiles() {
-			try {
-				final List<File> files = new ArrayList<>();
-				java.nio.file.Files.walkFileTree(start, new SimpleFileVisitor<Path>() {
-					@Override public FileVisitResult visitFile(Path file, BasicFileAttributes attributes) throws IOException {
-						Path name = file.getFileName();
-						if(name!=null&&matcher.matches(name))
-							files.add(file.toFile());
-						return CONTINUE;
-					}
-				}); return files.toArray(new File[0]);
-			} catch (IOException e) { return null; }
 		}
 	}
 
