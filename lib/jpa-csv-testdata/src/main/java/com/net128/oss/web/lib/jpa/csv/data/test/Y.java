fb277546c043adcc45d07c16799929ed594add93
michael <michael@mac>
2023-06-18 01:10:05 +0200

    ...

--- lib/jpa-csv-testdata/src/main/java/com/net128/oss/web/lib/jpa/csv/data/test/Y.java
+++ lib/jpa-csv-testdata/src/main/java/com/net128/oss/web/lib/jpa/csv/data/test/Y.java
@@ -1,36 +1,24 @@
 package com.net128.oss.web.lib.jpa.csv.data.test;
 
+import com.net128.oss.web.lib.jpa.csv.Identifiable;
 import com.net128.oss.web.lib.jpa.csv.util.Props;
-import lombok.Data;
+import lombok.*;
 
 import javax.persistence.*;
-import java.util.Objects;
 
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
-@Data
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Props.Sortable
 @Props.Hidden
-public class Y {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Props.Hidden
-    private Long id;
-
+public class Y extends Identifiable {
     @Column
     private String yattr1;
     
     @Column
     private String yattr2;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Y)) return false;
-        return Objects.equals(id, ((Y)o).id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
 }
